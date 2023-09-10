fbd09e2b7bab6597dfac497c18d28c19e48936c0
mikias-worash <mikias.worash.a@gmail.com>
2023-09-10 23:15:36 +0300

    navigation setup

---
+++ app/login/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div className="text-center">Login</div>;
+}
+
+export default page;
---
+++ app/profile/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Profile() {
+  return <div className="text-center">Profile</div>;
+}
+
+export default Profile;
