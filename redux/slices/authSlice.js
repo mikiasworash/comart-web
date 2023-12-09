ee68f297a012e759eb406567f2fbe9e84cb3be67
mikiasworash <mikias.worash.a@gmail.com>
2023-12-09 17:32:28 +0300

    updated userinfo retrieval

--- redux/slices/authSlice.js
+++ redux/slices/authSlice.js
@@ -1,9 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  userInfo: localStorage.getItem("userInfo")
-    ? JSON.parse(localStorage.getItem("userInfo"))
-    : null,
+  userInfo:
+    typeof window !== "undefined"
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
 };
 
 const authSlice = createSlice({
@@ -12,11 +13,15 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
-      localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      if (typeof window !== "undefined") {
+        localStorage.setItem("userInfo", JSON.stringify(action.payload));
+      }
     },
     logout: (state, action) => {
       state.userInfo = null;
-      localStorage.removeItem("userInfo");
+      if (typeof window !== "undefined") {
+        localStorage.removeItem("userInfo");
+      }
     },
   },
 });
